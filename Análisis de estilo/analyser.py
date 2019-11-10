f821a75cef5c3a0782327533c55ce198ca49c28d
Carlos Moreno Morera <carmor06@ucm.es>
2019-11-11 00:09:33 +0100

    Implementando el Analizador

--- Análisis de estilo/analyser.py
+++ Análisis de estilo/analyser.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import quotaunits as qu
 import messageextractor as mex
 import threadextractor as tex
+import threading
 
 NUM_RESOURCE_PER_LIST = 100
 
@@ -15,14 +16,18 @@ class Analyser:
             sent_lbl = self.service.users().labels().get(userId = 'me', id = 'SENT').execute()
             self.quota -= qu.LABELS_GET
 
-            cost_msg_ext = self.__calculate_resource_cost(qu.THREADS_LIST,
-                                            sent_lbl['messagesTotal'], qu.MSG_GET)
-            cost_thrd_ext = self.__calculate_resource_cost(qu.THREADS_LIST,
-                                            sent_lbl['threadsTotal'], qu.THREADS_GET)
+            self.num_msg = sent_lbl['messagesTotal']
+            self.num_thrd = sent_lbl['threadsTotal']
+
+            cost_msg_ext = self.__get_res_cost(qu.MSG_LIST, self.num_msg, qu.MSG_GET)
+            cost_thrd_ext = self.__get_res_cost(qu.THREADS_LIST, self.num_thrd, qu.THREADS_GET)
+
             if (cost_msg_ext <= cost_thrd_ext):
                 self.extractor = mex.MessageExtractor(self.service, self.quota)
             else:
                 self.extractor = tex.ThreadExtractor(self.service, self.quota)
+        self.msg_list = []
+        self.cv = threading.Condition()
 
-    def __calculate_resource_cost(self, listcost, numres, getcost):
+    def __get_res_cost(self, listcost, numres, getcost):
         return listcost * (numres // NUM_RESOURCE_PER_LIST + 1) + getcost * numres
