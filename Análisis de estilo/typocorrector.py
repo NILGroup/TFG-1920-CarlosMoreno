07003aff038a511a80567ec384b07731ecd403f8
Carlos Moreno Morera <carmor06@ucm.es>
2019-12-24 17:10:23 +0100

    Estructura concurrente implementada

--- Análisis de estilo/typocorrector.py
+++ Análisis de estilo/typocorrector.py
@@ -11,12 +11,13 @@ from csv import DictWriter
 
 class TypoCorrector:
     
-    def __init__(self, prep, corrected, prep_cv, corrected_cv, prep_fin, nlp):
+    def __init__(self, prep, corrected, prep_cv, cor_cv, prep_fin, typo_fin, nlp):
         self.prep = prep
         self.corrected = corrected
         self.prep_cv = prep_cv
-        self.corrected_cv = corrected_cv
+        self.corrected_cv = cor_cv
         self.pre_fin = prep_fin
+        self.typo_fin = typo_fin
         self.nlp = nlp
         
     def correct_msgs(self, user):
@@ -34,4 +35,27 @@ class TypoCorrector:
             csvfile = open(user + '/Preprocessing/preprocessed.csv', 'a')
             writer = DictWriter(csvfile, fieldnames = csv_columns)
         
+        self.prep_cv.aquire()
+        while (not(self.pre_fin.is_set()) or len(self.prep) > 0):
+            while (len(self.prep) == 0 and not(self.pre_fin.is_set())):
+                self.prep_cv.wait()
+            if (len(self.prep) != 0):
+                prep_msg = self.prep.pop()
+            self.prep_cv.release()
+            
+            if (not(self.pre_fin.is_set())):
+                msg_typo = {}
+                                
+                self.corrected_cv.acquire()
+                self.corrected.append(msg_typo)
+                self.corrected_cv.notify()
+                self.corrected_cv.release()
+                
+                self.prep_cv.acquire()
         
+        self.prep_cv.release()
+        
+        self.corrected_cv.acquire()
+        self.typo_fin.set()
+        self.corrected_cv.notify()
+        self.corrected_cv.release()
