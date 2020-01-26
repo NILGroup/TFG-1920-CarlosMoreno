eedeee073aa9c71ca4f7b43fdc3db1d6ef87c451
Carlos Moreno Morera <carmor06@ucm.es>
2020-01-27 00:34:40 +0100

    Implementadas las métricas de dificultad y stop words

--- Análisis de estilo/stylemeter.py
+++ Análisis de estilo/stylemeter.py
@@ -81,7 +81,7 @@ class StyleMeter:
         # Creates a dictionary of information for each sentence
         metrics['metricsSentences'] = [met_sents for i in range(numSentences)]
         
-    def __calculate_metrics_punct(self, metrics, m_sent, open_brackets, t):
+    def __calculate_punct_metrics(self, metrics, m_sent, open_brackets, t):
         """
         Modifies the metrics dictionary according to the given token which should
         be a token punctuation.
@@ -99,7 +99,8 @@ class StyleMeter:
             
         Returns
         -------
-        int: open_brackets (which could be modified in the function)
+        int: open_brackets (which could be modified in the function).
+        
         """
         if t.is_bracket and t.is_left_punct:
             open_brackets += 1
@@ -121,6 +122,122 @@ class StyleMeter:
             m_sent['num3Dots'] += 1
         
         return open_brackets
+    
+    def __calculate_word_metrics(self, metrics, m_sent, words, t):
+        """
+        Modifies the metrics dictionary according to the given token which should
+        be a non punctuation token.
+        
+        Parameters
+        ----------
+        metrics: dict
+            Dictionary where the metrics are going to be stored.
+        m_sent: dict
+            Dictionary of the metrics of the sentence where the given token is.
+        words: dics
+            Dictionary which stores how many times each word appears.
+        t: Spacy's Token or MyToken class
+            Token which is going to be analysed.
+            
+        Returns
+        -------
+        None.
+        
+        """
+        if t.lemma_ != '' and not(t.lemma_ in words):
+            words[t.lemma_] = 0
+                    
+        if not(len(t.text) in metrics['wordLength']):
+            metrics['wordLength'][len(t.text)] = 0
+            m_sent['wordLength'][len(t.text)] = 0
+                    
+        metrics['wordLength'][len(t.text)] += 1
+        m_sent['wordLength'][len(t.text)] += 1
+        words[t.lemma_] += 1
+                    
+        if t.is_stop:
+            metrics['numStopWords'] += 1
+            m_sent['numStopWords'] += 1
+        if t.pos_ in cfs.POS:
+            metrics[t.pos_] += 1
+            m_sent[t.pos_] += 1
+            
+    def __calculate_sentence_metrics(self, metrics, m_sent, s):
+        """
+        Modifies the metrics dictionary according to the given sentence.
+        
+        Parameters
+        ----------
+        metrics: dict
+            Dictionary where the metrics are going to be stored.
+        m_sent: dict
+            Dictionary of the metrics of the sentence.
+        s: dics
+            Dictionary which stores sentence information.
+            
+        Returns
+        -------
+        None.
+        
+        """
+        m_sent['numWords'] = len(s['words'])
+        m_sent['charLength'] = len(s['doc'].text)
+            
+        if not(m_sent['numWords'] in metrics['sentNumWords']):
+            metrics['sentNumWords'][m_sent['numWords']] = 0
+        if not(m_sent['charLength'] in metrics['sentLength']):
+            metrics['sentLength'][m_sent['charLength']] = 0
+                
+        metrics['sentNumWords'][m_sent['numWords']] += 1
+        metrics['sentLength'][m_sent['charLength']] += 1
+        
+    def __lambdaFK(self, metrics):
+        """
+        Calculates the lambda parameter of the expression of level of difficulty
+        of a text, given by Dubay, W.H.(2004). The principles of readability.
+        Costa Mesa, CA: Impact Information. Retrieved from
+        http://files.eric.ed.gov/fulltext/ED490073.pdf
+        
+        Lambda is the mean of one-syllable words per 100 words. I have considered
+        the words with length of 1 and 2 as one-syllable words.
+        
+        Parameters
+        ----------
+        metrics: dict
+            Dictionary where the metrics are stored.
+            
+        Returns
+        -------
+        float: lambda parameter.
+        
+        """
+        one_syllable = metrics['wordLength'][1] + metrics['wordLength'][2]
+        return (one_syllable * 100)/metrics['numWords']
+    
+    def __Flesch_Kincaid_Index(self, metrics):
+        """
+        Calculates the Flesch-Kincaid index (expression which determines the level
+        of difficulty of a text) given by Dubay, W.H.(2004). The principles of 
+        readability. Costa Mesa, CA: Impact Information. Retrieved from
+        http://files.eric.ed.gov/fulltext/ED490073.pdf
+        
+        Parameters
+        ----------
+        metrics: dict
+            Dictionary where the metrics are stored.
+            
+        Returns
+        -------
+        float: Flesch-Kincaid index.
+        
+        """
+        beta = 0
+        for key in metrics['sentNumWords']:
+            beta += key * metrics['sentNumWords'][key]
+        
+        beta = beta/len(metrics['metricsSentences'])
+        
+        return 1.599 * self.__lambdaFK(metrics) - 1.015 * beta - 31.517
         
     def __calculate_metrics(self, metrics, cor_msg, doc):
         self.__initialize_metrics(metrics, len(cor_msg['sentences']))
@@ -137,35 +254,21 @@ class StyleMeter:
                     t = cor['token']
                     
                 if t.is_punct:
-                    open_brackets = self.__calculate_metrics_punct(metrics, m_sent, 
+                    open_brackets = self.__calculate_punct_metrics(metrics, m_sent, 
                                                                    open_brackets, t)
                 elif not(t.like_url or t.like_email):
-                    if not(t.text in words):
-                        words[t.text] = 0
-                    
-                    if not(len(t.text) in metrics['wordLength']):
-                        metrics['wordLength'][len(t.text)] = 0
-                        m_sent['wordLength'][len(t.text)] = 0
-                    
-                    metrics['wordLength'][len(t.text)] += 1
-                    m_sent['wordLength'][len(t.text)] += 1
-                    words[t.text] += 1
+                    self.__calculate_word_metrics(metrics, m_sent, words, t)
             
-            m_sent['numWords'] = len(s['words'])
-            m_sent['charLength'] = len(s['doc'].text)
-            
-            if not(m_sent['numWords'] in metrics['sentNumWords']):
-                metrics['sentNumWords'][m_sent['numWords']] = 0
-            if not(m_sent['charLength'] in metrics['sentLength']):
-                metrics['sentLength'][m_sent['charLength']] = 0
-                
-            metrics['sentNumWords'][m_sent['numWords']] += 1
-            metrics['sentLength'][m_sent['charLength']] += 1
-            
+            self.__calculate_sentence_metrics(metrics, m_sent, s)
             ind_sent += 1
         
         metrics['numWords'] = len(doc)
         metrics['charLength'] = cor_msg['charLength']
+        
+        # Ril, Y., Y.C. & Fonseca, E. (2014) Determination of writing styles
+        metrics['stopRatio'] = (metrics['numStopWords']/metrics['numWords']) * 100
+        # Flesch-Kincaid (DuBay, 2004)
+        metrics['difficultyLevel'] = self.__Flesch_Kincaid_Index(metrics)
         
     def measure_style(self, user):
         if not os.path.exists(user + '/TypoCorrection'):
